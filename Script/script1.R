df3c5cdb9c21ecff3275d5d1c1affdc0032c9364
LizethMoreno <josselyn.moreno@epn.edu.ec>
2023-07-30 19:46:45 -0500

    se agregó una función empresa_1 que permite adjuntar variables y crear nuevas

--- Script/script1.R
+++ Script/script1.R
@@ -16,18 +16,39 @@ Formulario_102 <- read_excel("Data/Formulario 102.xls")
 #Parte 1 - Data: En esta sección deberás describir y mostrar cómo está conformado el dataset balance_2014.xlsx.
 str(balances_2014)
 head(balances_2014)
-#1.1
+#1.1 Utilizando los datos en el archivo llamado balance_2014.xlsx genera un tibble que denomines empresas y contenga las siguientes variables:
 empresas<-tibble::as_tibble(balances_2014) 
-empresas_1<-balances_2014 %>% select(Empresas=nombre_cia, Status=situacion, TipoEmpresa = tipo, Pais=pais, 
-                                     Provincia=provincia, Cantón=canton, Ciudad=ciudad,Actividad_economica=ciiu4_nivel1,
-                                     Subactividad=ciiu4_nivel6)
-empresas_2<-empresas %>%  mutate(LiquidezCorriente= (v312/v313)) %>% select(Empresas=nombre_cia, Status=situacion, TipoEmpresa = tipo, Pais=pais, 
-                                     Provincia=provincia, Cantón=canton, Ciudad=ciudad,Actividad_economica=ciiu4_nivel1,
-                                     Subactividad=ciiu4_nivel6) %>% View('prueba2')
+class(empresas)
+#1.2 empresas deberá tener las siguientes variables:
+empresas_1<-empresas %>%  mutate(LiquidezCorriente= (v312/5)) %>% 
+                          mutate(EndeudamientoActivo= (v312/6))  %>%
+                          mutate(EndeudamientoPatrimonial= (v312/6))  %>%
+                          mutate(EndeudamientoActivoFijo= (v312/6))  %>%
+                          mutate(Apalancamiento= (v312/6))  %>%
+                          select(Empresas=nombre_cia, 
+                                 Status=situacion,
+                                 TipoEmpresa = tipo, 
+                                 Pais=pais, 
+                                 Provincia=provincia, 
+                                 Canton=canton, 
+                                 Ciudad=ciudad,
+                                 Actividad_economica=ciiu4_nivel1,
+                                 SubActividad=ciiu4_nivel6,
+                                 LiquidezCorriente,
+                                 EndeudamientoActivo,
+                                 EndeudamientoPatrimonial,
+                                 EndeudamientoActivoFijo,
+                                 Apalancamiento)
 
 
-                                     
-                                     
+
+
+
+
+
+
+
+
 
 
 
